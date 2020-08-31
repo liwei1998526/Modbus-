d5f6215ca47035dddc6818a8cfc7148f4e2b875b
DESKTOP-S68MFEQ\ASUS <1310833646@qq.com>
2020-08-31 14:14:53 +0800

    提交头文件

--- ModbusTCP/Modbus.h
+++ ModbusTCP/Modbus.h
@@ -5,3 +5,4 @@ void WriteCoilCommand(int &m, int &n, vector<int>&val);
 void WriteRigisterCommand(int &m, int &n, vector<int>&val);
 string DEtoHEX(int n);
 char *FUNCTION01(char *s, char* send_buf, int k, vector<int>val);
+char *FUNCTION03(char *s, char* send_buf, int k, vector<int>val);
