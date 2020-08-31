ac0e467e292ab3407edb055ed62eaf510541317f
DESKTOP-S68MFEQ\ASUS <1310833646@qq.com>
2020-08-31 14:04:11 +0800

    完成01功能码编写以及10进制转16进制

---
+++ ModbusTCP/Modbus.h
@@ -0,0 +1,7 @@
+#include<vector>
+#include<string>
+using namespace std;
+void WriteCoilCommand(int &m, int &n, vector<int>&val);
+void WriteRigisterCommand(int &m, int &n, vector<int>&val);
+string DEtoHEX(int n);
+char *FUNCTION01(char *s, char* send_buf, int k, vector<int>val);
