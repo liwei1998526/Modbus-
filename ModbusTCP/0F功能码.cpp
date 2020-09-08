e288bb6189c23681550321d6b57dc271990eba9a
DESKTOP-3B459BJ\13108 <liwei1998526@gmail.com>
2020-09-08 19:54:30 +0800

    完善代码，添加03异常功能码

--- ModbusTCP/0F功能码.cpp
+++ ModbusTCP/0F功能码.cpp
@@ -2,7 +2,6 @@
 #include"Modbus.h"
 #include<vector>
 #include<iostream>
-using namespace std;
 char *FUNCTION0F(string recv_str, char* send_buf, int k, vector<int>&val)
 {
 	char send_buf_ret[600];
@@ -29,13 +28,24 @@ char *FUNCTION0F(string recv_str, char* send_buf, int k, vector<int>&val)
 		cout << "无可用功能码" << endl;
 		cout << "响应报文为：" << send_buf << endl;
 		strcpy(send_buf_ret, send_buf);
-		return send_buf_ret;
 	}
 	vector<int>ALL_DATA;
 	string recv_start_add = recv_str.substr(16, 4);
 	int start_add = stoi(recv_start_add, 0, 16);
 	string recv_digit = recv_str.substr(20, 4);
 	int digit = stoi(recv_digit, 0, 16);
+	string data_recv = recv_str.substr(24, recv_str.size() - 24);
+	string data_len = recv_str.substr(22, 2);
+	int data_len_int = stoi(data_len, 0, 16);
+	int digitlength;
+	if (digit % 8 == 0 && digit >= 8)
+	{
+		digitlength = digit / 8;
+	}
+	else
+	{
+		digitlength = (digit / 8) + 1;
+	}
 	if ((start_add + digit) > (k + val.size()) || start_add < k)
 	{
 		string send;
@@ -56,6 +66,23 @@ char *FUNCTION0F(string recv_str, char* send_buf, int k, vector<int>&val)
 		send_buf = (char*)send.c_str();
 		strcpy(send_buf_ret, send_buf);
 		cout << "寄存器超限" << endl;
+		cout << "响应报文为：" << send_buf_ret << endl;
+		return send_buf_ret;
+	}
+	else if (data_len_int * 2 != data_recv.size() || digitlength != data_recv.size())
+	{
+		string send;
+		for (int i = 0; i < 18; i++)
+		{
+			send += recv_str[i];
+		}
+		send[11] = '3';
+		send[14] += 8;
+		send[16] = '0';
+		send[17] = '3';
+		send_buf = (char*)send.c_str();
+		strcpy(send_buf_ret, send_buf);
+		cout << "非法数据值" << endl;
 		cout << "响应报文为：" << send_buf_ret << endl;
 		return send_buf_ret;
 	}
