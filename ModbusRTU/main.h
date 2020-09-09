0237eabf5176b9e8043c3c05c26d16b519904fd3
DESKTOP-3B459BJ\13108 <liwei1998526@gmail.com>
2020-09-09 12:56:16 +0800

    修改RTU格式

--- ModbusRTU/main.h
+++ ModbusRTU/main.h
@@ -11,7 +11,6 @@ string DEtoBIN(int n);
 unsigned short CRC16(unsigned char* puchMsg, unsigned short usDataLen);
 void HexstrtoByte(const char* source, unsigned char* dest, int sourcelen);
 char* hex2str(UINT8 *hex, int len);
-//UINT16 CRC_16(UINT8 *Data, unsigned int CRC_Len);
 char* function01(string add, string code);
 char* function0F(string add, string code);
 char* function10(string add, string code);
