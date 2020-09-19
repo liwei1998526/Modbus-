f1ced186ff8e1012c35f84c4fa3ad38c9315843d
DESKTOP-3B459BJ\13108 <liwei1998526@gmail.com>
2020-09-19 13:27:52 +0800

    增加公共功能

---
+++ public/HEXtostring.cpp
@@ -0,0 +1,35 @@
+#include<iomanip>
+#include<string>
+#include<windows.h>
+#include<vector>
+#include"public.h"
+using namespace std;
+char* hex2str(UINT8 *hex, int len)
+{
+	char str_ret[600];
+	char* str;
+	vector<string>Hex;
+	string ALL_data;
+	for (int j = 0; j < len; j++)
+	{
+		string data = DEtoHEX(hex[j]);
+		while (1)
+		{
+			if (data.size() < 2)
+			{
+				data = "0" + data;
+				continue;
+			}
+			break;
+		}
+		Hex.push_back(data);
+		data.clear();
+	}
+	for (int i = 0; i < Hex.size(); i++)
+	{
+		ALL_data += Hex[i];
+	}
+	str = (char*)ALL_data.c_str();
+	strcpy(str_ret, str);
+	return str_ret;
+}
